a92e96aaf17328dfa8101177cd0ecc5821c7d7e5
SupreeyaKokheng <163708882+SupreeyaKokheng@users.noreply.github.com>
2025-01-06 22:06:42 +0700

    add ui login

--- apartment-management/src/App.js
+++ apartment-management/src/App.js
@@ -1,16 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WaterMeterForm from './components/WaterMeterForm';
 import RoomList from './components/RoomList';
+import Login from "./components/Login";
+//import Welcome from "./Welcome";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <RoomList/>
-        //<WaterMeterForm />
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/RoomList" element={<RoomList />} />
+
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
+//import logo from './logo.svg';
+
+//import './App.css';
+//import WaterMeterForm from './components/WaterMeterForm';
+//import RoomList from './components/RoomList';
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//      <RoomList/>
+//        //<WaterMeterForm />
+//      </header>
+//    </div>
+//  );
+//}
+//
+//export default App;
