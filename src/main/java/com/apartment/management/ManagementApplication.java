f9ef5194cbea342124dc9140eb3b8a14fd43d3c0
SupreeyaKokheng <163708882+SupreeyaKokheng@users.noreply.github.com>
2025-02-01 17:21:57 +0700

    fix login check password

--- src/main/java/com/apartment/management/ManagementApplication.java
+++ src/main/java/com/apartment/management/ManagementApplication.java
@@ -9,10 +9,30 @@ public class ManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagementApplication.class, args);
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		// รหัสผ่านที่ต้องการทดสอบ
+		String rawPassword = "dream1234";
+		String storedPassword = "$2a$10$llRHzGj.aq57sHY0v2.y.ef.ta/Dg.y7CP480lPjtDHvUFhdWzW8.";
+
+		// ทดสอบการ match
+		boolean isMatch = encoder.matches(rawPassword, storedPassword);
+		System.out.println("🔍 passwordEncoder.matches: " + isMatch);
 //		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//		String rawPassword = "password123"; // 🔹 รหัสผ่านต้นฉบับ
+//
+//		// ทดสอบเข้ารหัสรหัสผ่านใหม่ ว่ามันแปลงแล้วจับคู่กันได้ไหม
+//		String rawPassword = "dream1234";
 //		String encodedPassword = encoder.encode(rawPassword);
+//
 //		System.out.println("🔐 Encoded Password: " + encodedPassword);
+//
+//		// ทดสอบเทียบกับรหัสผ่านในฐานข้อมูล
+//		boolean isMatch = encoder.matches(rawPassword, "$2a$10$KXH9umow4lctMK7wYqg.EePwo0jiqs9qd3u05H.Mge2GTDDIIcXYm");
+//		System.out.println("🔍 passwordEncoder.matches: " + isMatch);
+
+//		String encodedPassword = encoder.encode("dream1234");
+//		System.out.println("🔐 Encoded Password: " + encodedPassword);
+
 	}
 
 }
